262845c38c68e7e2afd700023bc571eec91f46ab
Krampit Maksim <75555341+SrVage@users.noreply.github.com>
2021-12-13 18:41:58 +0700

    AddModel

---
+++ WebStore/Models/Employer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace WebStore.Models
+{
+    public class Employer
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public int Age { get; set; }
+        public int TelephoneNumber { get; set; }
+        public string City { get; set; }
+
+        public Employer(int iD, string lastName, string firstName, string middleName, int age, int telephoneNumber, string city)
+        {
+            ID = iD;
+            LastName = lastName;
+            FirstName = firstName;
+            MiddleName = middleName;
+            Age = age;
+            TelephoneNumber = telephoneNumber;
+            City = city;
+        }
+    }
+}
+
