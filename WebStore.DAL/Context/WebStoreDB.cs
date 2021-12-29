54d8cec9bc98829be1f9908bd54c309ec697433b
Krampit Maksim <75555341+SrVage@users.noreply.github.com>
2021-12-29 21:58:29 +0700

    AddDBContext

---
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL.Context;
+
+public class WebStoreDB:DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Section> Sections { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+    public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options)
+    {
+        
+    }
+}
