4ea4b3c3f2dee4b5b414fea7d549700096606b47
Maksim Krampit <m.krampit@toothybirds.com>
2021-12-30 20:16:06 +0700

    FirstMigration

--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -10,12 +10,10 @@ public class Product:NamedEntity, IOrderedEntity
 {
     public int Order { get; set; }
     public int SectionID { get; set; }
-    [ForeignKey(nameof(SectionID))]
     public Section Section { get; set; }
     public int? BrandID { get; set; }
-    [ForeignKey(nameof(BrandID))]
     public Brand Brand { get; set; }
     public string ImageURL { get; set; }
-    [Column(TypeName = "decimal(18,2")]
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
