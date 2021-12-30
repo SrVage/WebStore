8c6cc47c654bbb933a0f01eb4292cd08f91dedce
Krampit Maksim <75555341+SrVage@users.noreply.github.com>
2021-12-30 17:06:44 +0700

    ConnectToDataBase

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,9 +1,13 @@
-﻿using WebStore.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Services;
 using WebStore.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 services.AddControllersWithViews();
+services.AddDbContext<WebStoreDB>(o 
+    => o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 services.AddSingleton<IEmployerData, EmployerDataMemoryService>();
 services.AddSingleton<IProductData, InMemoryProductData>();
 var app = builder.Build();
