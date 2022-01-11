842ecfb6952ce6753b37ed61f74168cc9c086cb9
Maksim Krampit <savage_jawa@mail.ru>
2022-01-11 21:21:41 +0700

    RegisterNewUser

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,14 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModels.Identity;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register() => View();
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public async Task<IActionResult> Register()
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
 
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var user = new User
+            {
+                UserName = model.UserName,
+            };
+            var registerResult = await _userManager.CreateAsync(user, model.Password);
+            if (registerResult.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+            foreach (var error in registerResult.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View(model);
         }
         public IActionResult Login() => View();
         public IActionResult Logout() => RedirectToAction("Index", "Home");
