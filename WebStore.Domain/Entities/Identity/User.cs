c74c5e5ce4d7d2cf414779a382411e9bf43b6753
Maksim Krampit <savage_jawa@mail.ru>
2022-01-14 22:25:51 +0700

    InitializeDataOfIdentitySystem

--- WebStore.Domain/Entities/Identity/User.cs
+++ WebStore.Domain/Entities/Identity/User.cs
@@ -4,5 +4,8 @@ namespace WebStore.Domain.Entities.Identity
 {
     public class User:IdentityUser
     {
+        public const string Administrator = "Admin";
+        public const string DefaultAdminPassword = "Admin_123";
+
     }
 }
