76e52f9de5846f34657c1114b753fb97b883605f
Maksim Krampit <savage_jawa@mail.ru>
2022-01-17 17:43:55 +0700

    EditAndDeleteProducts

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Areas.Admin.ViewModels;
 using WebStore.Domain.Entities.Identity;
 using WebStore.Services.Interfaces;
 
@@ -19,6 +20,67 @@ namespace WebStore.Areas.Admin.Controllers
         {
             var products = _productData.GetProduct();
             return View(products);
+        }
+
+        public IActionResult Edit(int ID)
+        {
+            var product = _productData.GetProductByID(ID);
+
+            if (product is null)
+                return NotFound();
+            return View(new EditProductViewModel
+            {
+                Id = product.ID,
+                Name = product.Name,
+                Order = product.Order,
+                SectionId = product.SectionID,
+                Section = product.Section.Name,
+                Brand = product.Brand?.Name,
+                BrandId = product.BrandID,
+                ImageUrl = product.ImageURL,
+                Price = product.Price,
+            });
+        }
+        [HttpPost]
+        public IActionResult Edit(EditProductViewModel Model)
+        {
+            if (!ModelState.IsValid)
+                return View(Model);
+
+            var product = _productData.GetProductByID(Model.Id);
+            if (product is null)
+                return NotFound();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int ID)
+        {
+            var product = _productData.GetProductByID(ID);
+
+            if (product is null)
+                return NotFound();
+
+            return View(new EditProductViewModel
+            {
+                Id = product.ID,
+                Name = product.Name,
+                Order = product.Order,
+                SectionId = product.SectionID,
+                Section = product.Section.Name,
+                Brand = product.Brand?.Name,
+                BrandId = product.BrandID,
+                ImageUrl = product.ImageURL,
+                Price = product.Price,
+            });
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int ID)
+        {
+            var product = _productData.GetProductByID(ID);
+            if (product is null)
+                return NotFound();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
