7d26ceb99d9a499d67afe9063465096a843a5b04
Maksim Krampit <savage_jawa@mail.ru>
2022-01-17 16:56:58 +0700

    ProductsEditorIndex

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
+    [Area("Admin"), Authorize(Roles = Role.Administrators)]
     public class ProductsController : Controller
     {
+        private readonly IProductData _productData;
+
+        public ProductsController(IProductData productData)
+        {
+            _productData = productData;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _productData.GetProduct();
+            return View(products);
         }
     }
 }
