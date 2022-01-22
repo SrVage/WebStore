2d3ce7b4a260ba0a9120ee93c0f28b1396332f4a
Maksim Krampit <savage_jawa@mail.ru>
2022-01-22 12:38:50 +0700

    AddValueClient

--- Services/WebStore.WepAPI.Clients/Values/ValuesClient.cs
+++ Services/WebStore.WepAPI.Clients/Values/ValuesClient.cs
@@ -1,6 +1,37 @@
 ﻿using WebStore.Interfaces.TestAPI;
 namespace WebStore.WepAPI.Clients.Values;
-    internal class ValuesClient:IValuesService
-    {
+internal class ValuesClient : IValuesService
+{
+    private HttpClient _client;
 
+
+    public void Add(string value)
+    {
+        throw new NotImplementedException();
     }
+
+    public int Count()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(int ID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Edit(int ID, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetByID(int ID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<string> GetValues()
+    {
+        throw new NotImplementedException();
+    }
+}
