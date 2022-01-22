221ced9aa922be4e1b438051489ed50644622214
Maksim Krampit <savage_jawa@mail.ru>
2022-01-22 12:38:29 +0700

    BaseClient

---
+++ Services/WebStore.Interfaces/TestAPI/IValuesService.cs
@@ -0,0 +1,12 @@
+﻿namespace WebStore.Interfaces.TestAPI
+{
+    public interface IValuesService
+    {
+        IEnumerable<string> GetValues();
+        int Count();
+        string GetByID(int ID);
+        void Add(string value);
+        void Edit(int ID, string value);
+        bool Delete(int ID);
+    }
+}
---
+++ Services/WebStore.WepAPI.Clients/Values/ValuesClient.cs
@@ -0,0 +1,6 @@
+﻿using WebStore.Interfaces.TestAPI;
+namespace WebStore.WepAPI.Clients.Values;
+    internal class ValuesClient:IValuesService
+    {
+
+    }
