c09a6086827a472975457aaaec5fcfcdfaaa2edb
Maksim Krampit <savage_jawa@mail.ru>
2022-01-24 15:06:48 +0700

    FixedBaseWebAPIClient

---
+++ Services/WebStore.WepAPI.Clients/Employers/EmployersClient.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.WepAPI.Clients.Base;
+
+namespace WebStore.WepAPI.Clients.Employers
+{
+    public class EmployersClient : BaseClient
+    {
+        public EmployersClient(HttpClient client, string address) : base(client, "api/employers")
+        {
+        }
+    }
+}
