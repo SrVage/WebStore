9c4e9c5a2c5df4e4e829694709dab264b1cdf2ba
Krampit Maksim <75555341+SrVage@users.noreply.github.com>
2022-02-05 13:41:54 +0700

    TestIndexWithMock

--- Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using WebStore.Controllers;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
 using Assert = Xunit.Assert;
 
 namespace WebStore.Tests.Controllers
@@ -8,6 +14,16 @@ namespace WebStore.Tests.Controllers
 	[TestClass]
 	public class HomeControllerTests
 	{
+		[TestMethod]
+		public void IndexReturnsViewResult()
+        {
+			var mockProductData = new Mock<IProductData>();
+			mockProductData.Setup(s => s.GetProduct(It.IsAny<ProductFilter>())).Returns<ProductFilter>(f => Enumerable.Empty<Product>());
+			var controller = new HomeController();
+			var actualResult = controller.Index(mockProductData.Object);
+			Assert.IsType<ViewResult>(actualResult);
+		}
+
 		[TestMethod]
 		public void ConfiguredActionReturnsStringValue()
         {
