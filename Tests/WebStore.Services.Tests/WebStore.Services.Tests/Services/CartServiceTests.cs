2d45726bf5d0cf685ce14d30e099a49d8ab8e57f
Krampit Maksim <75555341+SrVage@users.noreply.github.com>
2022-02-05 16:31:34 +0700

    AddServicesTestProject

---
+++ Tests/WebStore.Services.Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Services;
+using Assert = Xunit.Assert;
+namespace WebStore.Services.Tests.Services
+{
+	public class CartServiceTests
+	{
+
+	}
+}
+
