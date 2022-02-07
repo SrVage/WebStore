a38b02726e3eea5b18e1e300a9ec8585956a259f
Krampit Maksim <75555341+SrVage@users.noreply.github.com>
2022-02-07 11:58:35 +0700

    PrepareTagHelper

---
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore.TagHelpers
+{
+    [HtmlTargetElement(Attributes=AttributeName)]
+    public class ActiveRoute : TagHelper
+    {
+        private const string AttributeName = "is-active-route";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll(AttributeName);
+        }
+    }
+}
+
