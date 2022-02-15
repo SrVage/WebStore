08d6e00671559a04cbdfac4d9489e9bdc8e6c85f
MKrampit <MKrampit@bitbucket.org>
2022-02-15 13:02:47 +0700

    AddPagingTagHelper

--- Common/WebStore.Domain/ViewModels/PageViewModel.cs
+++ Common/WebStore.Domain/ViewModels/PageViewModel.cs
@@ -6,6 +6,7 @@ namespace WebStore.Domain.ViewModels
 		public int Page { get; set; }
 		public int PageSize { get; set; }
 		public int TotalItems { get; set; }
+		public int TotalPages => PageSize == 0 ? 0 : (int)Math.Ceiling((double)TotalItems / PageSize);
 	}
 }
 
