745ed5c55c80a9758b724897c5d5eefad6c3ebda
MKrampit <MKrampit@bitbucket.org>
2022-02-15 22:35:03 +0700

    RetryPolitic

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -16,6 +16,8 @@ using WebStore.Logging;
 using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Json;
+using Polly;
+using Polly.Extensions.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 //builder.Logging.AddLog4Net();
@@ -43,7 +45,9 @@ services.AddHttpClient("WebStoreAPIIdentity", client => client.BaseAddress = new
    .AddTypedClient<IUserTwoFactorStore<User>, UsersClient>()
    .AddTypedClient<IUserClaimStore<User>, UsersClient>()
    .AddTypedClient<IUserLoginStore<User>, UsersClient>()
-   .AddTypedClient<IRoleStore<Role>, RolesClient>();
+   .AddTypedClient<IRoleStore<Role>, RolesClient>()
+   .AddPolicyHandler(GetRetryPolicy());
+
 
 services.Configure<IdentityOptions>(opt =>
 {
@@ -84,7 +88,24 @@ services.AddHttpClient("WebStoreAPI", client => client.BaseAddress = new(configu
 .AddTypedClient<IValuesService, ValuesClient>()
 .AddTypedClient<IEmployerData, EmployersClient>()
 .AddTypedClient<IProductData, ProductsClient>()
-.AddTypedClient<IOrderService, OrdersClient>();
+.AddTypedClient<IOrderService, OrdersClient>()
+.AddPolicyHandler(GetRetryPolicy())
+.AddPolicyHandler(GetCircuitBreakerPolicy());
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy(int MaxRetryCount = 5, int MaxJitterTime = 1000)
+{
+    var jitter = new Random();
+    return HttpPolicyExtensions
+       .HandleTransientHttpError()
+       .WaitAndRetryAsync(MaxRetryCount, RetryAttempt =>
+            TimeSpan.FromSeconds(Math.Pow(2, RetryAttempt)) +
+            TimeSpan.FromMilliseconds(jitter.Next(0, MaxJitterTime)));
+}
+
+static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy() =>
+    HttpPolicyExtensions
+       .HandleTransientHttpError()
+       .CircuitBreakerAsync(handledEventsAllowedBeforeBreaking: 5, TimeSpan.FromSeconds(30));
 
 var app = builder.Build();
 
